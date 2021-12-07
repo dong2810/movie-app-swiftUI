bdc70c5846c8822c39c3a347a94e003a5a676dfb
Dong <dong2810123@gmail.com>
2021-12-07 09:44:48 +0700

    Initial Commit

---
+++ firstMovie/firstMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstMovieApp.swift
+//  firstMovie
+//
+//  Created by đông on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct firstMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
