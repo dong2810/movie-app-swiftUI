a45810b0a54e2cfef48fbca7b8100b115d678bc6
dongnh99 <dongnh@vmodev.com>
2022-03-19 10:45:43 +0700

    first commit

---
+++ firstMovie/Views/SideMenu.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenu.swift
+//  firstMovie
+//
+//  Created by đông on 10/02/2022.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenu()
+    }
+}
